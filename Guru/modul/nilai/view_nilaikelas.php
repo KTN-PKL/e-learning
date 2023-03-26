2ebd3f924767af786562a371c6f9f6a96a6c19f6
febriananur <febriananurokhman@gmail.com>
2023-03-26 12:09:54 +0700

    add detail

--- Guru/modul/nilai/view_nilaikelas.php
+++ Guru/modul/nilai/view_nilaikelas.php
@@ -59,6 +59,7 @@ Daftar Nilai
         <div class="card-body">
           <h4 class="card-title">
         <a href="../Report/nilai/nilai_perkelas.php?ujian=<?=$_GET['ujian']; ?>&kelas=<?=$_GET['kelas']; ?>&jurusan=<?=$_GET['jurusan']; ?>" target="_blank" class="btn btn-dark text-white text-right"> <i class="fa fa-print text-white"></i> Print</a>
+
         <a href="../Guru/export.php?ID=<?=$_GET['ujian']; ?>" target="_blank" class="btn btn-success text-white text-right"> <i class="fa fa-file-excel-o text-white"></i> Export to Excell</a>
           </h4>
           <div class="table-responsive">
@@ -74,7 +75,7 @@ Daftar Nilai
 	<th>Salah</th>
 	<th>Kosong</th>
 	<th>NILAI</th>	
-	<th>Desc</th>	
+	<th>Action</th>	
 	</tr>
 	</thead>
 	<tbody>
@@ -91,20 +92,14 @@ Daftar Nilai
 		<td><?=$nli['jml_salah']; ?></td>
 		<td><?=$nli['jml_kosong']; ?></td>
 		<td><?=$nli['nilai']; ?></td>
-
-		<?php
-			$qnilai = mysqli_query($con,"SELECT * FROM nilai,tb_siswa WHERE id_ujian='$s[id_ujian]' AND id_kelas='[id_kelas]'");
-			var_dump($qnilai);
-			$tnilai = mysqli_num_rows($qnilai);
-			$rnilai = mysqli_fetch_array($qnilai);
-		?>
-		<td>
-			<a data-toggle="modal" data-target="#myModal<?php echo $s["id_ujian"]; ?>" href="<?php echo $s["id_ujian"]; ?>" class="badge badge-pill badge-info" style="font-size: 14px;">
-                <i class="fa fa-pencil"></i> View                      
-            </a> <?php } ?>
+			<td>
+		<a data-toggle="modal" data-target="#myModal<?php echo $r['id_siswa'] ?>" href="#" class="badge badge-pill badge-info" style="font-size: 14px;">
+                            <i class="fa fa-pencil"></i>
+                          Cek Jawaban                         
+		</a>
 		</td>
 	</tr>
-
+<?php } ?>
 	</tbody>
 	</table>
 
@@ -116,11 +111,13 @@ Daftar Nilai
 </div>
 </div>
 
-
-
-<div class="modal fade bs-example-modal-lg" id="myModal<?php echo $s[
-                          "id_ujian"
-                      ]; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<?php
+$tampil = mysqli_query($con, "SELECT * FROM tb_siswa WHERE id_kelas='$_GET[kelas]' AND id_jurusan='$_GET[jurusan]' ORDER BY nama_siswa ASC");
+while($r=mysqli_fetch_array($tampil)){
+    $id = $_GET['ujian'].",".$r['id_siswa'];
+?>
+       <!-- Modal -->
+	   <div class="modal fade bs-example-modal-lg" id="myModal<?php echo $r['id_siswa'] ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
                         <div class="modal-dialog modal-lg" role="document">
                           <div class="modal-content">
                             <div class="modal-header" style="margin-left: 25px;">
@@ -131,61 +128,72 @@ Daftar Nilai
                                  <tr>
                                   <td>MATA PELAJARAN</td>
                                   <td>:</td>
-                                  <td><b><?php echo $s["mapel"]; ?></b> </td>
+                                  <td><b><?php echo $du['mapel']; ?></b> </td>
                                 </tr>
                                  <tr>
-                                  <td>SEMESTER</td>
+						          <td>SEMESTER</td>
+						          <td>:</td>
+						          <td><?php echo $du['semester']; ?></td>
+					            </tr>
+					            <tr>
+						          <td>JENIS UJIAN</td>
+						          <td>:</td>
+						          <td><?php echo $du['jenis_ujian']; ?></td>
+					           </tr>
+                                 <tr>
+                                  <td>Nama Siswa</td>
                                   <td>:</td>
-                                  <td><b><?php echo $s["semester"]; ?></b> </td>
+                                  <td><b><?php echo $r['nama_siswa'] ?></b> </td>
                                 </tr>
                             </table>
                             </div>
                             <div class="modal-body" style="overflow: scroll;height: 500px;background-color: #fff;margin-left: 50px;">
-                            <?php
+                            <?php 
                             $no = 1;
-                            $tampil = mysqli_query(
-                                $con,
-                                "SELECT * FROM soal WHERE id_ujian='$s[id_ujian]' ORDER BY id_soal DESC"
-                            );
-                            $jawaban = mysqli_query(
-                                $con,
-                                "SELECT * FROM nilai WHERE id_ujian='$s[id_ujian]' AND id_siswa='$_SESSION[id_siswa]' "
-                            );
-                            $isi = mysqli_fetch_array($jawaban);
-                            $pt = explode(",", $isi["acak_soal"]);
-                            $jwb = explode(",", $isi["jawaban"]);
-                            $k = 0;
+                            $tampil = mysqli_query($con, "SELECT * FROM soal WHERE id_ujian='$_GET[ujian]' ORDER BY id_soal DESC");
+                            $jawaban = mysqli_query($con, "SELECT * FROM nilai WHERE id_ujian='$_GET[ujian]' AND id_siswa='$r[id_siswa]' ");
+                            $isi=mysqli_fetch_array($jawaban);
+                            $pt = explode(",", $isi['acak_soal']);
+                            $jwb = explode(",", $isi['jawaban']);
+                            $k=0;
                             foreach ($pt as $pts) {
-                                $a = $pts;
-                                $jwbn[$pts] = $jwb[$k];
-                                $k = $k + 1;
+                              $a = $pts;
+                              $jwbn[$pts]=$jwb[$k];
+                              $k =$k+1;
                             }
-                            while ($r = mysqli_fetch_array($tampil)) { ?>
+                            while($r=mysqli_fetch_array($tampil)){ ?>
                           
-                            <p><b><?= $no++ ?> .<?= $r["soal"] ?></b></p>
+                            <p><b><?=$no++; ?> .<?=$r['soal']; ?></b></p>
                             <ol type="A" style='font-size: 13px;'>    
-                            <?php
-                            $l = $r["id_soal"];
-                            for ($i = 1; $i <= 5; $i++) {
-                                $kolom = "pilihan_$i";
-                                if ($i == $jwbn[$l]) {
-                                    echo "<li style='font-size: 13px; color : red;'>$r[$kolom]</li>";
-                                } else {
-                                    echo "<li style='font-size: 13px;'>$r[$kolom]</li>";
-                                }
+                            <?php 
+                            $l=$r['id_soal'];
+                            
+                            for($i=1; $i<=5; $i++){ 
+                            $kolom = "pilihan_$i";
+                            if($i==$jwbn[$l]){
+                            echo "<li style='font-size: 13px; color : red;'>$r[$kolom]</li>";
+                            }else{ 
+                            echo "<li style='font-size: 13px;'>$r[$kolom]</li>";
                             }
+
+                            }
+
                             ?>
                            
                             </ol>
-                            <?php }
-                            $id = $s["id_ujian"] . "," . $_SESSION["id_siswa"];
+                            <?php } 
+                            
                             ?>
                             </div>
                             <div class="modal-footer">
                               <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                              <a href="../Report/soal/print_soal.php?ID=<?= $id ?>" target="_blank" class="btn btn-primary"> <i class="fa fa-download text-white"></i> Download Soal</a>
+                              <a href="../Report/soal/print_soal2.php?ID=<?=$id; ?>" target="_blank" class="btn btn-primary"> <i class="fa fa-download text-white"></i> Download Soal</a>
                             </div>
                           </div>
                         </div>
                       </div>
+                            
 
+<?php
+}
+?>
